b79eb9405a79d9c2fac25dd090b5f47953c46446
harrisonz7058 <harrisonz7058@gmail.com>
2026-03-02 13:36:39 -0500

    Update lab_1a.py
    -added introduction edit

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Harrison Zhu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi everyone! My name is Harrion Zhu and I'm a junior from New York. In my free time, I like playing any racket sports(tennis, badminton, ping pong, and pickleball). A fun fact about me is that I like oreo flavored things but I don't like eating oreos.")
 if __name__ == "__main__":
     main()
